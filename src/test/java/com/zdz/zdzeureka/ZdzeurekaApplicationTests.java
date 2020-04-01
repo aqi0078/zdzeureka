a46ceb41fa34c54a3f738dfe3b2b3ee30646d2d9
dezhi <584638114@qq.com>
2020-04-01 15:24:50 +0800

    update

---
+++ src/test/java/com/zdz/zdzeureka/ZdzeurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdz.zdzeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZdzeurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
